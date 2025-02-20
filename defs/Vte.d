643069a7de32ac136e258070c69df9cfc14fbcca
Dejan Lekic <dejanlekic@bmlltech.com>
2025-02-20 10:39:22 +0000

    As suggested by EG, disabled get_text* methods.

--- defs/Vte.d
+++ defs/Vte.d
@@ -18,3 +18,8 @@
 //# Set basic parameters to out
 //!set record[EventContext].method[get_coordinates].parameters.parameter[x][direction] out
 //!set record[EventContext].method[get_coordinates].parameters.parameter[y][direction] out
+
+//# Disable some problematic deprecated methods
+//!set class[Terminal].method[get_text][disable] 1
+//!set class[Terminal].method[get_text_include_trailing_spaces][disable] 1
+//!set class[Terminal].method[get_text_range][disable] 1
