e437b8f0175446ac79bb92841d29c85d3c725bc4
Dejan Lekic <dejanlekic@bmlltech.com>
2025-03-03 22:11:01 +0000

    Vte 2.91 added, providing VTE for the recently added Gtk 3 (#20)

---
+++ packages/vte2/vte/char_attributes.d
@@ -0,0 +1,27 @@
+module vte.char_attributes;
+
+import gid.gid;
+import vte.c.functions;
+import vte.c.types;
+import vte.types;
+
+class CharAttributes
+{
+  VteCharAttributes cInstance;
+
+  this(void* ptr, Flag!"Take" take = No.Take)
+  {
+    if (!ptr)
+      throw new GidConstructException("Null instance pointer for Vte.CharAttributes");
+
+    cInstance = *cast(VteCharAttributes*)ptr;
+
+    if (take)
+      safeFree(ptr);
+  }
+
+  void* cPtr()
+  {
+    return cast(void*)&cInstance;
+  }
+}
---
+++ packages/vte2/vte/types.d
@@ -0,0 +1,88 @@
+module vte.types;
+
+import gid.gid;
+import glib.error;
+import glib.types;
+import vte.c.functions;
+import vte.c.types;
+import vte.terminal;
+
+
+// Enums
+alias Align = VteAlign;
+alias CursorBlinkMode = VteCursorBlinkMode;
+alias CursorShape = VteCursorShape;
+alias EraseBinding = VteEraseBinding;
+alias FeatureFlags = VteFeatureFlags;
+alias Format = VteFormat;
+alias PtyError = VtePtyError;
+alias PtyFlags = VtePtyFlags;
+alias RegexError = VteRegexError;
+alias TextBlinkMode = VteTextBlinkMode;
+alias WriteFlags = VteWriteFlags;
+
+// Structs
+alias TerminalClassPrivate = VteTerminalClassPrivate*;
+
+// Callbacks
+alias SelectionFunc = bool delegate(vte.terminal.Terminal terminal, glong column, glong row);
+alias TerminalSpawnAsyncCallback = void delegate(vte.terminal.Terminal terminal, glib.types.Pid pid, glib.error.ErrorG error);
+
+/**
+ * The major version number of the VTE library
+ * $(LPAREN)e.g. in version 3.1.4 this is 3$(RPAREN).
+ */
+enum MAJOR_VERSION = 0;
+
+
+/**
+ * The micro version number of the VTE library
+ * $(LPAREN)e.g. in version 3.1.4 this is 4$(RPAREN).
+ */
+enum MICRO_VERSION = 4;
+
+
+/**
+ * The minor version number of the VTE library
+ * $(LPAREN)e.g. in version 3.1.4 this is 1$(RPAREN).
+ */
+enum MINOR_VERSION = 76;
+
+
+enum REGEX_FLAGS_DEFAULT = 1075314688;
+
+
+/**
+ * Use this as a spawn flag $(LPAREN)together with flags from #GSpawnFlags$(RPAREN) in
+ * [vte.pty.Pty.spawnAsync].
+ * Normally, the spawned process inherits the environment from the parent
+ * process; when this flag is used, only the environment variables passed
+ * to [vte.pty.Pty.spawnAsync] etc. are passed to the child process.
+ */
+enum SPAWN_NO_PARENT_ENVV = 33554432;
+
+
+/**
+ * Use this as a spawn flag $(LPAREN)together with flags from #GSpawnFlags$(RPAREN) in
+ * [vte.pty.Pty.spawnAsync].
+ * Prevents [vte.pty.Pty.spawnAsync] etc. from moving the newly created child
+ * process to a systemd user scope.
+ */
+enum SPAWN_NO_SYSTEMD_SCOPE = 67108864;
+
+
+/**
+ * Use this as a spawn flag $(LPAREN)together with flags from #GSpawnFlags$(RPAREN) in
+ * [vte.pty.Pty.spawnAsync].
+ * Requires [vte.pty.Pty.spawnAsync] etc. to move the newly created child
+ * process to a systemd user scope; if that fails, the whole spawn fails.
+ * This is supported on Linux only.
+ */
+enum SPAWN_REQUIRE_SYSTEMD_SCOPE = 134217728;
+
+
+enum TEST_FLAGS_ALL = 18446744073709551615;
+
+
+enum TEST_FLAGS_NONE = 0;
+
