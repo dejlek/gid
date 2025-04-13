d958df33c0020979811ecae07ce2640e2382f9ab
Element Green <element@kymorphia.com>
2025-04-13 11:27:12 -0600

    Remove unused webkitwebprocessextension6 UserMessage error_quark with missing error enum

--- defs/webkitwebprocessextension6.d
+++ defs/webkitwebprocessextension6.d
@@ -2,3 +2,6 @@
 //# #####################################
 
 //!gir WebKitWebProcessExtension-6.0
+
+//# Remove error_quark since it isn't used and is missing a GUserMessageError enum
+//!del class[UserMessage].function[error_quark]
